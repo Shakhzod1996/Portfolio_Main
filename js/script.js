67fccd8a182dfd72f266e390894cba3f1a473a56
Shakhzod1996 <shaxzod1996shax@gmail.com>
2022-06-22 18:45:04 +0500

    hello

--- js/script.js
+++ js/script.js
@@ -172,3 +172,8 @@ const lightbox=document.querySelector(".lightbox"),
             allSection[i].classList.toggle("open");
         }
     }
+
+    window.replainSettings = { id: '62fe0c12-67cb-43ea-9adc-f695be55aa89' };
+(function(u){var s=document.createElement('script');s.async=true;s.src=u;
+var x=document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);
+})('https://widget.replain.cc/dist/client.js');
